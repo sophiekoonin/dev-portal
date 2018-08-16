4a9139096284c4da980050aad8a2fc5b6b8fc193
Sophie Koonin <sophie.koonin@gmail.com>
2018-08-16 20:31:02 +0100

    Set up react-router with two basic views, dashboard and login - currently empty

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 
 import './App.css'
+import Dashboard from './views/Dashboard'
+import LoginPage from './views/LoginPage'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Developer Portal</h1>
-        </header>
-        <p className="App-intro">List of apps will go here.</p>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/login" component={LoginPage} />
+    </div>
+  </Router>
+)
 
 export default hot(module)(App)
---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Developer Portal</h1>
+    </header>
+    <p className="App-intro">List of apps will go here.</p>
+  </div>
+)
+
+export default Dashboard
