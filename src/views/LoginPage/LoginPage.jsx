e2fefcdd6aeaff21ae7adef27e9dc1484265180c
Sophie Koonin <sophie.koonin@gmail.com>
2018-08-17 17:36:40 +0100

    Build out basic login form

--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -1,18 +1,54 @@
 import React from 'react'
 
-const LoginPage = () => (
-  <div>
-    <h1>Log in</h1>
-    <div className="login-form">
-      <form>
-        <label htmlFor="login-user">Username</label>
-        <input id="login-user" required />
-        <label htmlFor="login-pass">Password</label>
-        <input id="login-pass" type="password" required />
-        <button id="login-submit">Submit</button>
-      </form>
-    </div>
-  </div>
-)
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
 
+  onUpdateUsername = e => {
+    this.setState({ username: e.target.value })
+  }
+  onUpdatePassword = e => {
+    this.setState({ password: e.target.value })
+  }
+
+  onSubmitLogin = e => {
+    e.preventDefault
+    this.props.onSubmitLogin(...this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Log in</h1>
+        <div className="login-form">
+          <form onSubmit={this.onSubmitLogin}>
+            <label htmlFor="login-user">Username</label>
+            <input
+              id="login-user"
+              required
+              value={this.state.username}
+              onChange={this.onUpdateUsername}
+            />
+            <label htmlFor="login-pass">Password</label>
+            <input
+              id="login-pass"
+              type="password"
+              required
+              value={this.state.password}
+              onChange={this.onUpdatePassword}
+            />
+            <button id="login-submit" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
 export default LoginPage
